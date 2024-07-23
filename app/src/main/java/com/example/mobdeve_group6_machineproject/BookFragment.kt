8a674c73a2ac243b17c69d1ed1f162ea144cb8ba
rvillaroman <raphael_villaroman@dlsu.edu.ph>
2024-07-23 18:16:18 +0800

    Initial Book Tickets commit

--- app/src/main/java/com/example/mobdeve_group6_machineproject/BookFragment.kt
+++ app/src/main/java/com/example/mobdeve_group6_machineproject/BookFragment.kt
@@ -1,33 +1,16 @@
 package com.example.mobdeve_group6_machineproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BookFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BookFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +20,43 @@ class BookFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_book, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spinnerMovie: Spinner = view.findViewById(R.id.spinnerMovie)
+        val spinnerLocation: Spinner = view.findViewById(R.id.spinnerLocation)
+        val spinnerTime: Spinner = view.findViewById(R.id.spinnerTime)
+        val spinnerDate: Spinner = view.findViewById(R.id.spinnerDate)
+        val spinnerTickets: Spinner = view.findViewById(R.id.spinnerTickets)
+
+        // Dummy data for spinners
+        val movies = arrayOf("Movie 1", "Movie 2", "Movie 3")
+        val locations = arrayOf("Location 1", "Location 2", "Location 3")
+        val times = arrayOf("10:00 AM", "01:00 PM", "04:00 PM", "07:00 PM")
+        val dates = arrayOf("2024-07-23", "2024-07-24", "2024-07-25")
+        val tickets = arrayOf("1 Ticket", "2 Tickets", "3 Tickets", "4 Tickets")
+
+        spinnerMovie.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, movies)
+        spinnerLocation.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, locations)
+        spinnerTime.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, times)
+        spinnerDate.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, dates)
+        spinnerTickets.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, tickets)
+
+        // Handle seat selection
+        val gridSeats: GridLayout = view.findViewById(R.id.gridSeats)
+        for (i in 0 until gridSeats.childCount) {
+            val button = gridSeats.getChildAt(i) as Button
+            button.setOnClickListener {
+                button.isSelected = !button.isSelected
+                button.setBackgroundColor(
+                    if (button.isSelected) android.graphics.Color.RED else android.graphics.Color.LTGRAY
+                )
             }
+        }
+
+        val btnProceedToPayment: Button = view.findViewById(R.id.btnProceedToPayment)
+        btnProceedToPayment.setOnClickListener {
+            // Handle proceed to payment
+        }
     }
 }
